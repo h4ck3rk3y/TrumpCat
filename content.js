58c9b201fc44bcf8cae3524c4401a456d014a7ee
Gyanendra Mishra <anomaly.the@gmail.com>
2016-06-18 06:20:50 +1100

    best till now

--- content.js
+++ content.js
@@ -24,32 +24,24 @@ function determineIfTrump(url, callback, errorCallback){
 
 $(document).ready(function() {
     $('img').each(function(index, image){
-    	$(image).load(function(){
-    		console.log("loaded");
-	        var url = $(image).attr('src');
+        var url = $(image).attr('src');
 
-	        var original_style = $(image).html();
-	        var width = $(image).width();
-	        var height= $(image).height();
-	        var hourglass = chrome.extension.getURL("hourglass.gif");
+        var original_style = $(image).attr("style");
+        $(image).attr("style", "background:url(" + url + ")");
+        $(image).attr("src", chrome.extension.getURL("hourglass.gif"));
 
-	        var replacement = "<div style='width=" + width + "px; height="+height + "px; overflow:hidden;'><img style='background:url(" + url  + ");' src=" + hourglass + "></div>";
-	        console.log(replacement);
+        determineIfTrump(url, function(response){
+        	if (response.isTrump == true){
+        		$(image).attr('src', response.cat);
+        	}
+        	else{
+        		$(image).attr("src",  url);
+        	}
+        	$(image).attr("style", original_style);
+        }, function(){
 
-
-	        $(image).replaceWith(replacement);
-
-	        // $(image).attr("style", "background:url(" + url + ")");
-	        // $(image).attr("src", chrome.extension.getURL("hourglass.gif"));
-
-	        determineIfTrump(url, function(response){
-	        	if (response.isTrump == true){
-	        		$(image).attr('src', response.cat);
-	        	}
-	        	$(image).replaceWith(original_style);
-	        }, function(){
-	           	$(image).replaceWith(original_style);
-        	})
-    	});
-	});
+        	$(image).attr("style", original_style);
+        	$(image).attr("src", url);
+        })
+    });
 });
