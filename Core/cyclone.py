9c45adb78b3b25fc25a953a39ae14c5f6468d12b
Gyanendra Mishra <anomaly.the@gmail.com>
2016-06-19 00:50:14 +0530

    overhall

---
+++ Core/cyclone.py
@@ -0,0 +1,28 @@
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, RequestHandler, Application
+from api  import app
+import tornado.httpserver
+import tornado.options
+
+tornado.options.parse_command_line()
+
+
+
+class MainHandler(RequestHandler):
+  def get(self):
+    self.write("Tornado")
+
+tr = WSGIContainer(app)
+
+application = Application([
+(r"/tornado", MainHandler),
+(r".*", FallbackHandler, dict(fallback=tr)),
+],debug=True)
+
+http_server = tornado.httpserver.HTTPServer(application, ssl_options = {"certfile": "/etc/letsencrypt/live/gyani.xyz/cert.pem", "keyfile": "/etc/letsencrypt/live/gyani.xyz/privkey.pem"})
+
+
+if __name__ == "__main__":
+  http_server.listen(443)
+  IOLoop.instance().start()
