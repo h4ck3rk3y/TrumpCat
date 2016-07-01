6e939c5dd87483b737c89409a69a29a479c265a3
Gyanendra Mishra <anomaly.the@gmail.com>
2016-07-02 01:57:36 +0530

    better

--- Core/api.py
+++ Core/api.py
@@ -1,60 +1,52 @@
-from flask import Flask, Response, request, jsonify
+from flask import request, url_for
+from flask.ext.api import FlaskAPI, status, exceptions
 from pymongo import MongoClient
 from alchemyapi import AlchemyAPI
 from PIL import Image
 import cStringIO
 import urllib
-import requests
 
 from test import *
 
-app = Flask(__name__)
-app.config.update(
-    DEBUG = True,
-)
 
+app = FlaskAPI(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client.trumpcat
 urls = db.urls
 
 @app.route("/url", methods=["POST"])
-def isTrump(url = None):
+def isTrump():
 
-    if request.method == 'POST':
-        url = request.args.get('url', '')
-        exists = urls.find({'url':url})
+	if not request.json.has_key('url'):
+		return {"isTrump": False, "message" : "no url specified"}
 
-        if exists.count() != 0:
-            trump = exists[0]
+	url = request.json['url']
+	exists = urls.find({'url':url})
 
-            if trump["isTrump"]:
-                return {'isTrump': True, "cat": trump["cat"]}
-            else:
-                return {"isTrump": False, "message":"set false in db"}
+	if exists.count() != 0:
+		trump = exists[0]
 
-        # alchemyapi = AlchemyAPI()
-        # response = alchemyapi.faceTagging("url", url)
+		if trump["isTrump"]:
+			return {'isTrump': True, "cat": trump["cat"]}
+		else:
+			return {"isTrump": False, "message":"set false in db"}
 
-        response = doIt(url)
 
-        # if response.has_key('imageFaces'):
-        #   for i in response['imageFaces']:
-        #       if i.has_key('identity') and  i['identity']['name'] == "Donald Trump":
+	response = doIt(url)
 
-        if response == 1:
-            file = cStringIO.StringIO(urllib.urlopen(url).read())
-            img = Image.open(file)
-            width = img.size[0]
-            height = img.size[1]
-            cat = "https://placekitten.com/%s/%s" %(str(width), str(height))
-            urls.insert({'url': url, 'cat': cat, "isTrump": True})
-            return jsonify(**{"isTrump": True, "cat": cat})
+	if response == 1:
+		file = cStringIO.StringIO(urllib.urlopen(url).read())
+		img = Image.open(file)
+		width = img.size[0]
+		height = img.size[1]
+		cat = "https://placekitten.com/%s/%s" %(str(width), str(height))
+		urls.insert({'url': url, 'cat': cat, "isTrump": True})
+		return {"isTrump": True, "cat": cat}
 
-        elif response == 0:
-            urls.insert({'url': url, 'isTrump': False, "message": "found false in db"})
-            return jsonify(**{'isTrump': False})
-
-        else:
-            urls.insert({'url': url, 'isTrump': False, "message": "request did not complete"})
-            return jsonify(**{"isTrump": False})
+	elif response == 0:
+		urls.insert({'url': url, 'isTrump': False, "message": "found false in db"})
+		return {'isTrump': False}
+	else:
+		urls.insert({'url': url, 'isTrump': False, "message": "request did not complete"})
+		return {"isTrump": False}
 
